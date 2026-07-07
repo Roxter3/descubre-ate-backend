e9d0edf4d825653344d1f0f9bc80f9d514f8fc7a
Jhonatan <jhonatanaparicio123@gmail.com>
2026-07-07 18:35:23 -0500

    Configurar CORS para Vercel

--- config/cors.php
+++ config/cors.php
@@ -23,6 +23,8 @@ $origenesPermitidos = [
     'http://localhost:3000',  // por si usas otro puerto común
     'http://127.0.0.1:5173',
     'http://127.0.0.1:5174',
+
+    'https://artesanias-psi.vercel.app'
 ];
 
 $origenSolicitante = $_SERVER['HTTP_ORIGIN'] ?? '';
